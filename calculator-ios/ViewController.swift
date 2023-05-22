7cac93ba77d0696a5aea6f65beaee9519af7f7f2
Kağan Değirmen <kagandegirmen@gmail.com>
2023-05-23 00:15:37 +0300

    UI fixed and added empty functionality to buttons

--- calculator-ios/ViewController.swift
+++ calculator-ios/ViewController.swift
@@ -8,11 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func modButtonClicked(_ sender: UIButton) {
     }
+    @IBAction func arithmeticSignClicked(_ sender: UIButton) {
+    }
+    @IBAction func allClearClicked(_ sender: UIButton) {
+    }
+    @IBAction func divideClicked(_ sender: UIButton) {
+    }
+    @IBAction func multiplicationClicked(_ sender: UIButton) {
+    }
+    @IBAction func minusClicked(_ sender: UIButton) {
+    }
+    @IBAction func plusClicked(_ sender: UIButton) {
+    }
+    @IBAction func equalsClicked(_ sender: UIButton) {
+    }
+    @IBAction func dotClicked(_ sender: UIButton) {
+    }
+    @IBAction func zeroClicked(_ sender: UIButton) {
+    }
+    @IBAction func oneClicked(_ sender: UIButton) {
+    }
+    @IBAction func twoClicked(_ sender: UIButton) {
+    }
+    @IBAction func threeClicked(_ sender: UIButton) {
+    }
+    @IBAction func fourClicked(_ sender: UIButton) {
+    }
+    @IBAction func fiveClicked(_ sender: UIButton) {
+    }
+    @IBAction func sixClicked(_ sender: UIButton) {
+    }
+    @IBAction func sevenClicked(_ sender: UIButton) {
+    }
+    @IBAction func eightClicked(_ sender: UIButton) {
+    }
+    @IBAction func nineClicked(_ sender: UIButton) {
+    }
+    
 
 
 }
